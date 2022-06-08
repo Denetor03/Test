5873b00ae97678344c9a133aa534a96978653058
Denetor03 <Daniel.alexander.siebel@gmail.com>
2022-06-08 21:46:30 +0200

    ✨ it works

--- js/game1.js
+++ js/game1.js
@@ -2,11 +2,14 @@ const allowedDirections = ["north", "south", "east", "west"];
 const allowedActions = ["move", "take", "drop", "examine", "help"];
 var currentRoom = 0;
 
-const north = {1: 2, 2:0, 6:5, 7:6, 6:5};
-const south = {0:2, 2:1, 5:6, 6:7};
-const east = {2:3, 3:4, 4:5, 5:8, 8:9};
-const west = {3:2, 4:3, 5:4, 8:5, 9:8};
 
+
+
+const north = {0:0, 1: 2, 2:0, 3:3, 4:4, 5:5, 6:5, 7:6, 8:8, 9:9};
+const south = {0:2, 1:1, 2:1, 3:3, 4:4, 5:6, 6:7, 7:7, 8:8, 9:9};
+const east = {0:0, 1:1, 2:3, 3:3, 4:5, 5:8, 6:7, 7:8, 8:9, 9:9};
+const west = {0:0, 1:1, 2:2, 3:2, 4:3, 5:4, 6:6, 7:7, 8:5, 9:8};
+const compass = {"north":north, "south":south, "east":east, "west":west};
 const description = {0 : "you are in a burning room, there are sparking wires everywhere",
                     1 : "It looks like an engine room, inactive.",
                     2 : "you are narrow hallway",
@@ -36,7 +39,7 @@ const Directions = {0 : "there is only one way to go(south)",
 document.getElementById("label1").innerHTML = description[currentRoom];
 document.getElementById("label2").innerHTML = Directions[currentRoom];
 var input = getInput();
-while(currentRoom != null)
+function main()
 {
     input = getInput();
     var inputArray = input.split(" ");
@@ -44,13 +47,13 @@ while(currentRoom != null)
     var direction = inputArray[1];
     if(allowedActions.indexOf(action) == -1)
     {
-        console.log("wtf do you want?");
+        document.getElementById("label3").innerHTML = "wtf do you want?";
     }
     else if(action == "move")
     {
         if(allowedDirections.indexOf(direction) == -1)
         {
-            console.log("I don't understand where you want to go.");
+            document.getElementById("label3").innerHTML = "I don't understand where you want to go.";
         }
         else
         {
@@ -61,7 +64,7 @@ while(currentRoom != null)
     {
         if(inputArray.length == 1)
         {
-            console.log("You need to tell me what you want to take.");
+            document.getElementById("label3").innerHTML = "You need to tell me what you want to take.";
         }
         else
         {
@@ -72,7 +75,7 @@ while(currentRoom != null)
     {
         if(inputArray.length == 1)
         {
-            console.log("You need to tell me what you want to drop.");
+            document.getElementById("label3").innerHTML = "You need to tell me what you want to drop.";
         }
         else
         {
@@ -94,9 +97,17 @@ while(currentRoom != null)
     {
         help();
     }
+    document.getElementById("label1").innerHTML = description[currentRoom];
+    document.getElementById("label2").innerHTML = Directions[currentRoom];
 }
 
 function getInput()
 {
     return document.getElementById("input").value;
+}
+
+function move(currentRoom, direction)
+{
+    var newRoom = compass[direction][currentRoom];
+    return newRoom;
 }
