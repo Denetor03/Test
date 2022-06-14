a03ca37668c82564a9fd62863757c4cabd198be3
Denetor03 <Daniel.alexander.siebel@gmail.com>
2022-06-14 23:44:51 +0200

    ✨ added base for weather page

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+const apiKey = "97430b983c734d0514c220ae2e152e99";
+let weather = {
+    apiKey: "97430b983c734d0514c220ae2e152e99",
+    fetchWeather: function (city) {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.apiKey}&units=metric`)
+            .then(response => response.json())
+            .then(( data ) => console.log(data));
+    },
+    displayWeather: function (data) {
+
+    }
+};
+
+
+function fetchWeather(city) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
+        .then(response => response.json())
+        .then(( data ) => console.log(data));
+};
+
+fetchWeather("solingen");
